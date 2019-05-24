d22ba525521232a77f5f6bf785af95472b421d4d
JoushuaHung <joushuahung@iq-t.com>
2019-05-24 18:29:35 +0800

    add delegate class

---
+++ Tutorial/MyDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tutorial
+{
+    public class MyDelegate
+    {
+        public delegate void MySayEvent(string str);
+
+        public static void HelloFun(string str)
+        {
+            Console.WriteLine("  Hello, {0}!", str);
+        }
+        public static void GoodbyeFun(string str)
+        {
+            Console.WriteLine("  Goodbye, {0}!", str);
+        }
+
+        public void RunDelegate(string str)
+        {
+            MySayEvent mySayEvent;
+            mySayEvent = new MySayEvent(HelloFun);
+            mySayEvent += new MySayEvent(GoodbyeFun);
+            mySayEvent -= new MySayEvent(HelloFun);
+
+            mySayEvent(str);
+        }
+    }
+}
