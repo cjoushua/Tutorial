e5f4b6c2243ed80c2200678183f475cd2017df86
JoushuaHung <joushuahung@iq-t.com>
2019-08-19 16:16:10 +0800

    add use NameValueCollection from JCollection.cs

---
+++ Tutorial/JCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class JCollection
+    {
+        JCollection()
+        {
+            NameValueCollection nvc = new NameValueCollection()
+            {
+              {"key1", "value1"},
+              {"key2", "value2"},
+              {"key3", "value3"},
+            };
+
+            string ans = nvc["key3"];
+
+            List<string> words = ans.Split(',').ToList();
+
+            ans = words[0];
+        }
+    }
+}
